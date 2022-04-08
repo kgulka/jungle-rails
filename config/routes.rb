37132c6814f4b34deed75a469eab9149e51aaec6
Kent Gulka <kentgulka@modindsoft.com>
2022-04-07 20:29:22 -0600

    Feature Add User Authentication - Route and Controller Changes
    ran the generate controller for user and added user to routes.

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,11 @@ Rails.application.routes.draw do
     resources :categories, except: [:edit, :update, :show, :destroy]
   end
 
+    # These routes will be for signup. The first renders a form in the browse, the second will 
+    # receive the form and create a user in our database using the data given to us by the user.
+    get '/signup' => 'users#new'
+    post '/users' => 'users#create'
+
   get "about" => "about#index"
 
   # The priority is based upon order of creation: first created -> highest priority.
